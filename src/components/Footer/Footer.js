b79ee9bc15ff8dc72a4774e5bf821b2694f16bc0
Ola Assem <o.assem@gmail.com>
2018-07-12 18:44:58 +0200

    centered footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,21 +5,14 @@ export default class Footer extends React.Component{
   render(){
     return(
       <footer>
-      <div className="row">
-        <div className="col-12">
-          <div className="footer-content">
-
-                <div className="githubIcon-container">
-                  <a href="https://github.com/olaassem/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
-                </div>
-              </div>
-
-                <div className="linkedinIcon-container">
-                  <a href="https://www.linkedin.com/in/ola-assem-7404a44/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
-                </div>
-              </div>
-
-      </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="footer-content">
+              <a className="githubIcon-container" href="https://github.com/olaassem/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
+              <a className="linkedinIcon-container" href="https://www.linkedin.com/in/ola-assem-7404a44/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
       </footer>
     )
   }
